7ad314771de82b14b7a2044276759a44761c9b04
BhavyaGoyal28 <86304555+BhavyaGoyal28@users.noreply.github.com>
2021-06-27 21:30:23 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from"react";
+import Header from "./Header";
+import Footer from "./Footer";
+import note from "./note";
+
+function App(){
+   return (
+     <div>
+<Header />
+<Footer />
+<Note />
+       </div>;
+   );
+}
+
+export default App;
